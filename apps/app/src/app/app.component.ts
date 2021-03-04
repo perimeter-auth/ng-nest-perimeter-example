8650ae72233080a5db7c3afb869e2b56d9b9e3ad
baio <max.putilov@gmail.com>
2021-03-04 20:08:06 +0300

    fix openid info

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '@ng-nest-perimeter-example/api-interfaces';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'ng-nest-perimeter-example-root',
@@ -9,5 +10,21 @@ import { Message } from '@ng-nest-perimeter-example/api-interfaces';
 })
 export class AppComponent {
   hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private readonly authService: OAuthService
+  ) {}
+
+  onAuthorize() {
+    this.authService.initLoginFlow(null, { prompt: 'none' });
+  }
+
+  onLogout() {
+    this.authService.logOut();
+  }
+
+  onAuthorizeSSO() {
+    //https://auth0.com/blog/oauth2-implicit-grant-and-spa/
+    // this.authService.initLoginFlow(null, { prompt: 'none' });
+  }
 }
--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -3,11 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthConfig, OAuthModule, OAuthService } from 'angular-oauth2-oidc';
+
+export const authCodeFlowConfig: AuthConfig = {
+  // Url of the Identity Provider
+  issuer: 'https://localhost:5001/api/auth/issuers/main/default/dev5',
+
+  // URL of the SPA to redirect the user to after login
+  redirectUri: window.location.origin,
+
+  // The SPA's id. The SPA is registerd with this id at the auth-server
+  // clientId: 'server.code',
+  clientId: 'y5TcVy7kKl28XBPKNN2FpajZjnsrxH4ghY',
+
+  responseType: 'code',
+
+  // set the scope for the permissions the client should request
+  // The first four are defined by OIDC.
+  // Important: Request offline_access to get a refresh token
+  // The api scope is a usecase specific one
+  scope: 'openid profile email offline_access',
+
+  showDebugInformation: true,
+  //strictDiscoveryDocumentValidation: false,
+  //skipIssuerCheck: true,
+};
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, OAuthModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(authService: OAuthService) {
+    authService.configure(authCodeFlowConfig);
+    authService.loadDiscoveryDocumentAndTryLogin();
+  }
+}
